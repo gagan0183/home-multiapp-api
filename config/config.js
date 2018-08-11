94ed2b47219cc138e39309e4323cafcb4727a1e8
Gagan <g.deepsingh1@gmail.com>
2018-08-11 23:48:09 +0530

    app

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var config  = {
   production: {
-    databaseUrl: 'mongodb://gagan:Arihant_90@ds119422.mlab.com:19422/heroku_cljj3dl'
+    databaseUrl: 'mongodb://gagan:Arihant_90@ds119422.mlab.com:19422/heroku_cljj3dlk'
   },
   local: {
     databaseUrl: 'mongodb://127.0.0.1:27017/homeapp'
